e63f3290d1baf902b8ba7e18d13878a4c27f4900
José Antonio Awad Jofré <jose.awad.jofre@gmail.com>
2017-07-08 17:06:00 -0400

    se actualiza modelo de clases

---
+++ web/model/abogado.class.php
@@ -0,0 +1,27 @@
+<?php
+    class Abogado{
+        
+        var  $id;
+        var  $rutNumero;
+        var  $nombreCompleto;
+        var  $fechaContratacion;
+        var  $valorHora;
+        
+        public static function crear($rutNumero, $nombreCompleto, $fechaContratacion, $valorHora){
+
+        }
+
+        public static function actualizar($id,$rutNumero, $nombreCompleto, $fechaContratacion, $valorHora){
+
+        }
+
+        public static function buscarPorId($id){
+
+        }
+
+        public static function buscarTodos(){
+            
+        }
+
+    }
+?>
---
+++ web/model/atencion.class.php
@@ -0,0 +1,26 @@
+<?php
+    class Atencion{
+
+        var $id;
+        var $fechaHora;
+        var $idAbogados;
+        var $idUsuarios;
+        var $estado;
+
+                public static function crear($fechaHora, $idAbogados, $idUsuarios, $estado){
+
+        }
+
+        public static function actualizar($id,$fechaHora, $idAbogados, $idUsuarios, $estado){
+
+        }
+
+        public static function buscarPorId($id){
+
+        }
+
+        public static function buscarTodos(){
+            
+        }
+    }
+?>
